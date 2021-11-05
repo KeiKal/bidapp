f1583df82e7c6023ad6b23d7abc6fa0b4de9115a
Bon Brian Rivera <bon.rivera@flyadeal.onmicrosoft.com>
2021-11-05 14:34:09 +0800

    added placeholders for bids

--- src/app/shared/image.service.ts
+++ src/app/shared/image.service.ts
@@ -16,9 +16,9 @@ export class ImageService {
 } 
 
 const IMAGES = [
-    {"id" : 1, "category": "finds", "description":"Gold Binoculars", "detail": "cumque error dolores saepe minima lorem dolorum perferendis", "url": "assets/img/finds_01.jpg" },
-    {"id" : 2, "category": "finds", "description":"Gold Old Camera", "detail": "Consequuntur LOREM, dolor tempora a ad quas ab molestias,",  "url": "assets/img/finds_02.jpg" },
-    {"id" : 3, "category": "pottery", "description":"Old Pottery Set", "detail": "cumque error dolores saepe minima repellat dolorum perferendis",  "url": "assets/img/pottery_01.png" },
-    {"id" : 4, "category": "relic", "description":"Pope Bone Relic", "detail": "laborum adipisci praesentium, corporis explicabo!",  "url": "assets/img/relic_01.jpg" },
-    {"id" : 5, "category": "relic", "description":"Maitreya Project", "detail": "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",  "url": "assets/img/relic_02.jpg" }
+    {"id" : 1, "category": "finds", "description":"Gold Binoculars", "detail": "cumque error dolores saepe minima lorem dolorum perferendis", "url": "assets/img/finds_01.jpg", "bids": 35.00 },
+    {"id" : 2, "category": "finds", "description":"Gold Old Camera", "detail": "Consequuntur LOREM, dolor tempora a ad quas ab molestias,",  "url": "assets/img/finds_02.jpg", "bids": 60.00 }, 
+    {"id" : 3, "category": "pottery", "description":"Old Pottery Set", "detail": "cumque error dolores saepe minima repellat dolorum perferendis",  "url": "assets/img/pottery_01.png","bids": 50.00 },
+    {"id" : 4, "category": "relic", "description":"Pope Bone Relic", "detail": "laborum adipisci praesentium, corporis explicabo!",  "url": "assets/img/relic_01.jpg","bids": 35.00 },
+    {"id" : 5, "category": "relic", "description":"Maitreya Project", "detail": "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",  "url": "assets/img/relic_02.jpg","bids": 45.00 }
 ]
